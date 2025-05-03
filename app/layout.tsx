09344a3e087ad76857cad05c27a61a7b5f12d5f2
a-gaoba <abdulrahman1997abdo@gmail.com>
2025-05-03 23:39:49 +0300

    first comment

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
       <body className={cn("overflow-x-hidden", tajawal.variable, arefRuqaa.variable, tajawal.className)}>
         <CartProvider>
           <div className="flex min-h-screen flex-col">
-            {/* <Header /> */}
+            <Header />
             <Suspense>
               <main className="flex-1">{children}</main>
             </Suspense>
